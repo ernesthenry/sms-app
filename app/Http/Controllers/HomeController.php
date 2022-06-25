831422cec04adb0ec6b677c62e02d60b40caa9ed
ernesthenry <henry38ernest@gmail.com>
2022-06-25 17:50:48 +0300

    sending of SMS using Twilio

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,13 +9,25 @@ class HomeController extends Controller
 {
     //
     public function storePhoneNumber(Request $request)
-{
-    //run validation on data sent in
-    $validatedData = $request->validate([
-        'phone_number' => 'required|unique:users_phone_number|numeric'
-    ]);
-    $user_phone_number_model = new UsersPhoneNumber($request->all());
-    $user_phone_number_model->save();
-    return back()->with(['success'=>"{$request->phone_number} registered"]);
-}
+    {
+        //run validation on data sent in
+        $validatedData = $request->validate([
+            'phone_number' => 'required|unique:users_phone_number|numeric'
+        ]);
+        $user_phone_number_model = new UsersPhoneNumber($request->all());
+        $user_phone_number_model->save();
+        return back()->with(['success' => "{$request->phone_number} registered"]);
+    }
+
+    private function sendMessage($message, $recipients)
+    {
+        $account_sid = getenv("TWILIO_SID");
+        $auth_token = getenv("TWILIO_AUTH_TOKEN");
+        $twilio_number = getenv("TWILIO_NUMBER");
+        $client = new Client($account_sid, $auth_token);
+        $client->messages->create(
+            $recipients,
+            ['from' => $twilio_number, 'body' => $message]
+        );
+    }
 }
